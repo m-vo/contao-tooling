fd1c85e9d0c786c6e4b65ecf4ea72a83a906cd8d
M. Vondano <m-vo@users.noreply.github.com>
2021-02-02 21:20:45 +0100

    fix db deploy steps

--- custom-recipe/database.php
+++ custom-recipe/database.php
@@ -11,6 +11,8 @@ task('database:dump', static function (): void {
 task('database:backup', static function (): void {
     if (!get('backup_database', true)) {
         writeln("\r\033[1A\033[32C … skipped");
+
+        return;
     }
 
     run('cd {{release_path}} && {{bin/php}} {{bin/console}} database:backup');
@@ -19,6 +21,8 @@ task('database:backup', static function (): void {
 task('database:import', static function (): void {
     if (!get('import_database', false)) {
         writeln("\r\033[1A\033[32C … skipped");
+
+        return;
     }
 
     run('cd {{release_path}} && {{bin/php}} {{bin/console}} database:import');
